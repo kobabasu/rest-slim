3a3863a9c41d79f0f64dc95d2c772492f8e0fe16
kobabasu <keiji.kobayashi.web@gmail.com>
2015-11-28 11:10:11 +0900

    Slimを実行

--- index.php
+++ index.php
@@ -5,6 +5,12 @@ namespace Api;
 require_once('vendor/autoload.php');
 /*}}}*/
 
+
+
+$app = new \Slim\Slim();
+$app->run();
+
+
 /*
 vim:ma:et:nu:ff=unix:fenc=utf-8:ft=php:ts=2:sts=0:sw=2:tw=60:fdm=marker:
  */
