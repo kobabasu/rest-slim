5eb91fec87e9fd38c63ee6f4b9f49b3e39b614db
kobabasu <keiji.kobayashi.web@gmail.com>
2015-12-09 04:11:11 +0900

    arrayに変換する行を追加

--- lib/Db/Put.php
+++ lib/Db/Put.php
@@ -3,6 +3,8 @@ namespace lib\Db;
 
 class Put extends Db {
   public function execute($sql, $values = array()) {
+    if (!is_array($values)) $values = array($values);
+
     try {
     } catch (PDOException $e) {
       $this->debug($e->getMessage());
--- lib/Db/post.php
+++ lib/Db/post.php
@@ -3,6 +3,8 @@ namespace Lib\Db;
 
 class Post extends Db {
   public function execute($sql, $values = array()) {
+    if (!is_array($values)) $values = array($values);
+    
     try {
       $stmt = $this->dbh->prepare($sql);
 
