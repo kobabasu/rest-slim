f2574e7ff02cf52e6da40ba72d5e0d16faf88d71
kobabasu <keiji.kobayashi.web@gmail.com>
2015-12-10 00:39:20 +0900

    getLastInsertIdメソッドを追加

--- lib/Db/post.php
+++ lib/Db/post.php
@@ -2,16 +2,27 @@
 namespace Lib\Db;
 
 class Post extends Db {
+
+  private $lastInsertId;
+
   public function execute($sql, $values = array()) {
     if (!is_array($values)) $values = array($values);
     
     try {
       $stmt = $this->dbh->prepare($sql);
 
-      return $stmt->execute($values);
+      $stmt->execute($values);
+
+      $this->lastInsertId = $this->dbh->lastInsertId();
+
+      return $stmt->rowCount();
 
     } catch (PDOException $e) {
       $this->debug($e->getMessage());
     }
+  }
+
+  public function getLastInsertId() {
+    return $this->lastInsertId;
   }
 }
