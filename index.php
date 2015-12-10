2c4c2f641cd6da59da612dbb464fb792e11fa80f
kobabasu <keiji.kobayashi.web@gmail.com>
2015-12-10 14:27:00 +0900

    slimのconfigでdebugを設定する必要があるためServerEnvironmentを移動

--- index.php
+++ index.php
@@ -1,21 +1,5 @@
 <?php
 
-// server environment and DEBUG /*{{{*/
-$ips = array(
-  // '10.0.2.15', // local
-  '150.60.6.11'
-);
-
-if (in_array($_SERVER['SERVER_ADDR'], $ips)) {
-  define('DEBUG', false);
-  $_ENV['SLIM_MODE'] = 'production';
-} else {
-  define('DEBUG', true);
-  $_ENV['SLIM_MODE'] = 'environment';
-}
-ini_set('display_errors', DEBUG);
-/*}}}*/
-
 // composer /*{{{*/
 require_once(__DIR__ . '/vendor/autoload.php');
 /*}}}*/
@@ -35,6 +19,24 @@ require_once(__DIR__ . '/config/db.php');
 
 
 $app = new \Slim\Slim();
+
+// server environment and DEBUG /*{{{*/
+$ips = array(
+  // '10.0.2.15', // local
+  '150.60.6.11'
+);
+
+if (in_array($_SERVER['SERVER_ADDR'], $ips)) {
+  $app->config('debug', false);
+  define('DEBUG', false);
+  $_ENV['SLIM_MODE'] = 'production';
+} else {
+  $app->config('debug', true);
+  define('DEBUG', true);
+  $_ENV['SLIM_MODE'] = 'environment';
+}
+ini_set('display_errors', DEBUG);
+/*}}}*/
 
 // Slim Extend /*{{{*/
 $app->render = new Lib\Slim\Render($app);
