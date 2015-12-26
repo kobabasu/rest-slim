0788066587437892aa0113c89162ed9c3eef0aef
kobabasu <keiji.kobayashi.web@gmail.com>
2015-12-26 15:15:30 +0900

    phpdocのコメントを追加

--- lib/Hello/Hello.php
+++ lib/Hello/Hello.php
@@ -1,11 +1,27 @@
 <?php
+/**
+ * Helloクラスのファイル
+ */
+
 namespace Lib\Hello;
 
+/**
+ * 一番単純なクラスのサンプル
+ */
 class Hello {
 
+  /**
+   * @ignore
+   */
   public function __construct() {
   }
 
+  /**
+   * 'Hello'を返す
+   *
+   * @access public
+   * @return String 'Hello'という文字列を返す
+   */
   public function say() {
     return 'Hello';
   }
