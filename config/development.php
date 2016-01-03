4db4f20e91d752097c8e2cb67e9f381f2dc08631
kobabasu <keiji.kobayashi.web@gmail.com>
2016-01-03 16:04:58 +0900

    configをdevelopment, productionでわける

--- config/development.php
+++ config/development.php
@@ -1,4 +1,8 @@
 <?php
+$_ENV['SLIM_MODE']  = 'development';
+$_ENV['DEBUG_MODE'] = true;
+$_ENV['LOG_ENABLE'] = false;
+
 const DB_HOST       = '0.0.0.0';
 const DB_USERNAME   = 'api';
 const DB_PASSWORD   = 'api012';
--- config/production.php
+++ config/production.php
@@ -1,4 +1,7 @@
 <?php
+$_ENV['SLIM_MODE']  = 'production';
+$_ENV['DEBUG_MODE'] = false;
+
 const DB_HOST       = '0.0.0.0';
 const DB_USERNAME   = 'api';
 const DB_PASSWORD   = 'api012';
