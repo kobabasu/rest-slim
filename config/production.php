f61d6fc18ce8006d069dcea407122f28ef4d6186
kobabasu <keiji.kobayashi.web@gmail.com>
2016-01-12 22:12:56 +0900

    configファイルを再編集

--- config/production.php
+++ config/production.php
@@ -5,9 +5,10 @@ const DEBUG_MODE       = false;
 $_ENV['SLIM_MODE']  = ENVIRONMENT_MODE;
 
 const DB_HOST       = '0.0.0.0';
+const DB_NAME       = 'api';
 const DB_USER       = 'api';
 const DB_PASS       = 'api012';
-const DB_NAME       = 'api';
+const DB_PORT       = '3306';
 
 const MAIL_HOST     = '127.0.0.1';
 const MAIL_PORT     = 1025;
