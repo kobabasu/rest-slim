b279950bd92a309b54dcecca27c0f355d9a028e5
kobabasu <keiji.kobayashi.web@gmail.com>
2016-01-14 23:04:53 +0900

    SwiftMailerのテストコードを追加

--- lib/SwiftMailer/Mailer.php
+++ lib/SwiftMailer/Mailer.php
@@ -1,163 +1,90 @@
 <?php
+/**
+ * Web App REST API
+ *
+ * @link https://github.com/kobabasu/rest-php.git
+ */
+
 namespace Lib\SwiftMailer;
+
+/**
+ * 実際にメールを送る
+ *
+ * @package SwiftMailer
+ */
 
 class Mailer
 {
-    const LOG_DIR = 'logs';
-    const INTERVAL_COUNT = 100;
-    const INTERVAL_TIME = 30;
+    /** @var Object #swift Swiftオブジェクト */
+    private $swift;
 
-    private $app;
-    private $charset;
-    private $transport;
-    private $mailer;
-    private $logger;
-    private $twig;
+    /** @var Object #message メッセージオブジェクト */
+    private $message;
 
-    private $subject;
-    private $from;
-    private $body;
-
-    public function __construct($app, $charset = null)
-    {
-        $this->app = $app;
-        $this->charset = $charset;
-        $this->setDefault();
-
-        $this->initSwift();
-        $this->initTwig();
-        $this->transport = $this->getTransport();
-        $this->setMailer();
-        $this->setAntiFlood();
-        $this->setLog();
+    /**
+     * Swiftオブジェクトを代入
+     *
+     * @param Object $swift
+     * @return void
+     * @codeCoverageIgnore
+     */
+    public function __construct(
+        \Lib\SwiftMailer\Init $swift
+    ) {
+        $this->swift = $swift;
     }
 
-    private function initSwift()
-    {
-        \Swift::init(function () {
-            \Swift_DependencyContainer::getInstance()
-                ->register('mime.qpheaderencoder')
-                ->asAliasOf('mime.base64headerencoder');
-            \Swift_Preferences::getInstance()->setCharset(
-                'iso-2022-jp'
-            );
-        });
-    }
-
-    private function initTwig()
-    {
-        $loader = new \Twig_Loader_Filesystem('mail');
-        $this->twig = new \Twig_Environment($loader, array(
-            'cache' => 'cache'
-        ));
-    }
-
-    private function setDefault()
-    {
-        $config = $this->app->config('mail');
-
-        $this->subject = $config['subject'];
-        $this->from    = $config['from'];
-        $this->body    = $config['body'];
-    }
-
-    private function getTransport()
-    {
-        $config = $this->app->config('smtp');
-
-        $transport = \Swift_SmtpTransport::newInstance(
-            $config['host'],
-            $config['port']
-        );
-
-        if ($config['user']) {
-            $transport->setUsername($config['user']);
-            $transport->setPassword($config['pass']);
-        }
-
-        return $transport;
-    }
-
-    public function setSubject($subject)
-    {
-        $this->subject = $subject;
-    }
-
-    public function setFrom($from)
-    {
-        $this->from = $from;
-    }
-
-    public function setBody($body)
-    {
-        $this->body = $body;
-    }
-
-    public function setTemplate($template, $data)
-    {
-        $this->body = $this->twig->render(
+    /**
+     * Twigのテンプレートを反映して返す
+     *
+     * @param String $template
+     * @param Array $data
+     * @return Object
+     */
+    public function setTemplate(
+        $template,
+        $data
+    ) {
+        return $this->swift->setTemplate(
             $template,
             $data
         );
     }
 
-    private function setMailer()
-    {
-        $mailer = \Swift_Mailer::newInstance($this->transport);
-        $this->mailer = $mailer;
-    }
-
-    private function setAntiFlood()
-    {
-        $this->mailer->registerPlugin(
-            new \Swift_Plugins_AntiFloodPlugin(
-                self::INTERVAL_COUNT,
-                self::INTERVAL_TIME
-            )
-        );
-    }
-
-    private function setLog()
-    {
-        $logger = new \Swift_Plugins_Loggers_ArrayLogger();
-        $this->mailer->registerPlugin(
-            new \Swift_Plugins_LoggerPlugin($logger)
+    /**
+     * メッセージを返す
+     *
+     * @param String $subject
+     * @param Array $from
+     * @param String $body
+     * @return Object
+     */
+    public function setMessage(
+        $subject,
+        $from,
+        $body
+    ) {
+        $this->message = $this->swift->setMessage(
+            $subject,
+            $from,
+            $body
         );
 
-        $this->logger = $logger;
+        return $this->message;
     }
 
-    public function saveLog()
-    {
-        $date = date('ymd');
-        $file = self::LOG_DIR . '/' . 'mail.' . $date . '.log';
-
-        $current = '';
-        if (file_exists($file)) {
-            $current .= file_get_contents($file);
-        }
-
-        $log = $this->logger->dump();
-        $current .= $log;
-
-        file_put_contents($file, $current, FILE_APPEND);
-    }
-
+    /**
+     * 実際に送る
+     *
+     * @param Array $to
+     * @return void
+     */
     public function send($to)
     {
-        $message = \Swift_Message::newInstance()
-            ->setTo($to)
-            ->setSubject($this->subject)
-            ->setFrom($this->from)
-            ->setBody($this->body);
+        $mailer = $this->swift->getMailer();
+        $this->message->setTo((Array)$to);
+        $res = $mailer->send($this->message);
 
-        if ($this->charset) {
-            $message->setCharset($this->charset);
-            $message->setEncoder(
-                \Swift_Encoding::get7BitEncoding()
-            );
-        }
-
-        $this->mailer->send($message);
+        return $res;
     }
 }
--- tests/lib/SwiftMailer/MailerTest.php
+++ tests/lib/SwiftMailer/MailerTest.php
@@ -5,12 +5,12 @@
  * @link https://github.com/kobabasu/rest-php.git
  */
 
-namespace Lib\Db;
+namespace Lib\SwiftMailer;
 
 /**
- * Postクラス用のテストファイル
+ * Mailerクラス用のテストファイル
  *
- * @package Db
+ * @package SwiftMailer
  */
 class MailerTest extends \PHPUnit_Framework_TestCase
 {
@@ -23,18 +23,14 @@ class MailerTest extends \PHPUnit_Framework_TestCase
      */
     protected function setUp()
     {
-        $pdo = new \Lib\Db\Connect(
+        $swift = new \Lib\SwiftMailer\Init(
             '127.0.0.1',
-            'api',
-            'api',
-            'api012',
-            '3306',
-            true
+            '1025',
+            null,
+            null
         );
-        $dbh = $pdo->getConnection();
-        $stub = new \Lib\Db\Post($dbh, true);
 
-        $this->object = $stub;
+        $this->object = new Mailer($swift);
     }
 
     /**
@@ -45,19 +41,66 @@ class MailerTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * executeが正しく返すか
+     * テンプレートを返すか
      *
-     * @covers Lib\Db\Post::execute()
-     * @test testExecuteFail()
+     * @covers Lib\SwiftMailer\Mailer::setTemplate()
+     * @test testSetTemplate()
      */
-    public function testExecuteFail()
+    public function testSetTemplate()
     {
-        try {
-            $sql = 'SELECT count(*) as `res` FROM `user`';
-            $res = $this->object->execute($sql);
-            $this->fail('fail');
-        } catch (\Exception $e) {
-            $this->assertEquals('fail', $e->getMessage());
-        }
+        $res = $this->object->setTemplate(
+            'defaultTest.twig',
+            array('name' => 'twig')
+        );
+
+        $this->assertEquals('Hello twig' . PHP_EOL, $res);
+    }
+
+    /**
+     * メッセージを返すか
+     *
+     * @covers Lib\SwiftMailer\Mailer::setMessage()
+     * @test testSetMessage()
+     */
+    public function testSetMessage()
+    {
+        $body = $this->object->setTemplate(
+            'defaultTest.twig',
+            array('name' => '太郎')
+        );
+
+        $res = $this->object->setMessage(
+            'test subject',
+            array('test@example.com' => 'テスト担当'),
+            $body
+        );
+
+        $this->assertInternalType('object', $res);
+    }
+
+    /**
+     * メッセージを返すか
+     *
+     * @covers Lib\SwiftMailer\Mailer::send()
+     * @test testSetSend()
+     */
+    public function testSend()
+    {
+        $body = $this->object->setTemplate(
+            'defaultTest.twig',
+            array('name' => '太郎')
+        );
+
+        $this->object->setMessage(
+            'タイトル',
+            array('admin@example.com' => 'テスト担当'),
+            $body
+        );
+
+        $res = $this->object->send(
+            'test@example.com'
+        );
+
+        $this->assertEquals(1, $res);
     }
 }
