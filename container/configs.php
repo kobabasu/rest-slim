b7b5659a5818e1184e10f3f4a24dbd52fa248afc
kobabasu <keiji.kobayashi.web@gmail.com>
2016-01-16 13:04:15 +0900

    Pimpleに登録

--- container/configs.php
+++ container/configs.php
@@ -2,31 +2,27 @@
 namespace Container;
 
 $c['config'] = function ($c) {
-    $config = array(
+    return array(
         'environment_mode' => ENVIRONMENT_MODE,
         'debug_mode' => DEBUG_MODE
     );
-    return $config;
 };
 
 $c['config.db'] = function ($c) {
-    $config = array(
+    return array(
         'host' => DB_HOST,
         'name' => DB_NAME,
         'user' => DB_USER,
         'pass' => DB_PASS,
         'port' => DB_PORT
     );
-    return $config;
 };
 
-$c['config.mailer'] = function ($c) {
-    $config = array(
+$c['config.mail'] = function ($c) {
+    return array(
         'host' => MAIL_HOST,
         'port' => MAIL_PORT,
         'user' => MAIL_USER,
-        'pass' => MAIL_PASS,
-        'logs' => true
+        'pass' => MAIL_PASS
     );
-    return $config;
 };
--- container/databases.php
+++ container/databases.php
@@ -1,10 +1,16 @@
 <?php
 namespace Container;
 
+use \Lib\Db\Connect;
+use \Lib\Db\Get;
+use \Lib\Db\Post;
+use \Lib\Db\Put;
+use \Lib\Db\Delete;
+
 $c['db.connection'] = function ($c) {
     $cfg = $c['config.db'];
 
-    $connect = new \Lib\Db\Connect(
+    $connect = new Connect(
         $cfg['host'],
         $cfg['name'],
         $cfg['user'],
@@ -14,4 +20,20 @@ $c['db.connection'] = function ($c) {
     );
 
     return $connect->getConnection();
+};
+
+$c['db.get'] = function ($c) {
+    return new Get($c['db.connection']);
+};
+
+$c['db.post'] = function ($c) {
+    return new Post($c['db.connection']);
+};
+
+$c['db.put'] = function ($c) {
+    return new Put($c['db.connection']);
+};
+
+$c['db.delete'] = function ($c) {
+    return new Delete($c['db.connection']);
 };
