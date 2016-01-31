24f9d0bec9a852646be2473a62b2ea5578c2a823
kobabasu <keiji.kobayashi.web@gmail.com>
2016-01-31 11:26:34 +0900

    コメントを追加

--- bootstrap.php
+++ bootstrap.php
@@ -4,18 +4,20 @@ use \Slim\App;
 /**
  * Slim
  */
+// import ./src/settings.php first
 $settings = require __DIR__ . '/src/settings.php';
 $app = new App($settings);
 
+// require ./src
 require __DIR__ . '/src/dependencies.php';
-
 require __DIR__ . '/src/middleware.php';
-
 require __DIR__ . '/src/app.php';
 
+// require ./routes
 $routeFiles = (array) glob('./routes/*.php');
 foreach ($routeFiles as $routeFile) {
     require $routeFile;
 }
 
+// then run
 $app->run();
