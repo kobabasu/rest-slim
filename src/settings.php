69148fd7463301bc9d7a2527d272f718e2b5f544
kobabasu <keiji.kobayashi.web@gmail.com>
2016-01-31 12:23:11 +0900

    configの定数を読み込む

--- src/settings.php
+++ src/settings.php
@@ -5,7 +5,24 @@ return array(
     'withJsonEncOption' => $withJsonEncOption,
 
     'settings' => array(
+        'environment' => ENVIRONMENT_MODE,
         'displayErrorDetails' => DEBUG
+    ),
+
+    'db' => array(
+        'host' => DB_HOST,
+        'name' => DB_NAME,
+        'user' => DB_USER,
+        'pass' => DB_PASS,
+        'port' => DB_PORT,
+        'charset' => DB_CHARSET
+    ),
+
+    'mail' => array(
+        'host' => MAIL_HOST,
+        'port' => MAIL_PORT,
+        'user' => MAIL_USER,
+        'pass' => MAIL_PASS
     ),
 
     'logger' => array(
