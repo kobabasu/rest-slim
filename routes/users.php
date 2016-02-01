5faf4144480ffbcab578a4c6ee88a0cdc46cef27
kobabasu <keiji.kobayashi.web@gmail.com>
2016-02-02 05:28:25 +0900

    POST, PUT, DELETEを追加

--- routes/users.php
+++ routes/users.php
@@ -14,15 +14,108 @@ $app->group('/users', function () {
             $response,
             $args
         ) {
-            $body = array('名前' => $args['name']);
-
             $db = $this->get('db.get');
-            $sql = 'select * from `users`;';
+            $sql = 'select * from `users`';
 
-            $body = $db->execute($sql);
+            if ($args['name']) {
+                $sql .= ' WHERE `name` = ?;';
+                $body = $db->execute($sql, $args['name']);
+            } else {
+                $body = $db->execute($sql);
+            }
 
             return $response->withJson(
                 $body,
+                200,
+                $this->get('settings')['withJsonEnc']
+            );
+        }
+    );
+
+    /**
+     * POST
+     */
+    $this->post(
+        '/',
+        function (
+            $request,
+            $response,
+            $args
+        ) {
+            $body = $request->getParsedBody();
+
+            $db = $this->get('db.post');
+
+            $sql  = 'INSERT INTO `users` ';
+
+            $fields = array_keys($body);
+            $values = array_values($body);
+            $holder = array_fill(0, count($values), '?');
+
+            $sql .= '(' . implode(', ', $fields) . ')';
+            $sql .= ' VALUES ';
+            $sql .= '(' . implode(', ', $holder) . ')';
+
+            $db->execute($sql, $values);
+
+            return $response->withJson(
+                $body,
+                200,
+                $this->get('settings')['withJsonEnc']
+            );
+        }
+    );
+
+    /**
+     * PUT
+     */
+    $this->put(
+        '/{id:[0-9]+}',
+        function (
+            $request,
+            $response,
+            $args
+        ) {
+            $body = $request->getParsedBody();
+
+            $db = $this->get('db.put');
+
+            $fields = array_keys($body);
+            $values = array_values($body);
+
+            $sql = 'UPDATE `users` SET ';
+            $sql .= implode(' = ?, ', $fields) . ' = ?';
+            $sql .= ' WHERE `id` = ' . (int)$args['id'];
+
+            $res = $db->execute($sql, $values);
+
+            return $response->withJson(
+                $res,
+                200,
+                $this->get('settings')['withJsonEnc']
+            );
+        }
+    );
+
+    /**
+     * DELETE
+     */
+    $this->delete(
+        '/{id:[0-9]+}',
+        function (
+            $request,
+            $response,
+            $args
+        ) {
+            $db = $this->get('db.delete');
+
+            $sql = 'DELETE FROM `users` ';
+            $sql .= 'WHERE `id` = ' . (int)$args['id'];
+
+            $res = $db->execute($sql);
+
+            return $response->withJson(
+                $res,
                 200,
                 $this->get('settings')['withJsonEnc']
             );
