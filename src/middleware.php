d53474d830bc726e2c534eb3fddecb9533d9d826
kobabasu <keiji.kobayashi.web@gmail.com>
2016-02-13 03:59:46 +0900

    slim-basic-authをadd

--- src/middleware.php
+++ src/middleware.php
@@ -3,4 +3,15 @@
  * application middleware
  */
 
+use \Slim\Middleware\HttpBasicAuthentication;
+
 // e.g: $app->add(enw \Slim\Csrf\Guard);
+
+/**
+ * tuupola/slim-basic-auth
+ */
+$app->add(new HttpBasicAuthentication([
+    'users' => [
+        'api' => 'api012'
+    ]
+]));
