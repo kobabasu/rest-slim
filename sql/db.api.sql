c68d692ca341bb108902f53eb2d8ebd8b88e2482
kobabasu <keiji.kobayashi.web@gmail.com>
2016-04-24 13:45:17 +0900

    profileを変更するようコメントを追加

--- sql/db.api.sql
+++ sql/db.api.sql
@@ -5,10 +5,12 @@
   */
 
 -- profile for local database;
+-- profile名、DB名を変更すること ユーザ情報は変更する必要なし
 let g:dbext_default_profile_api = 'type=mysql:user=api:passwd=api012:host=0.0.0.0:port=3306:dbname=api:extra=-vvv'
 DBSetOption profile=api
 
 -- profile for local testing database;
+-- profile名、DB名を変更すること ユーザ情報は変更する必要なし
 let g:dbext_default_profile_api_test = 'type=mysql:user=api:passwd=api012:host=0.0.0.0:port=3306:dbname=api_test:extra=-vvv'
 DBSetOption profile=api_test
 
