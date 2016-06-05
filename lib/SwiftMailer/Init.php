1b25ab4b9566fde9ee81f9ebb7f0281735f703b2
kobabasu <keiji.kobayashi.web@gmail.com>
2016-06-05 10:36:50 +0900

    setTemplateとしていたメソッドをUtilクラスとして分離 Twigを返すだけなので

--- lib/SwiftMailer/Init.php
+++ lib/SwiftMailer/Init.php
@@ -14,12 +14,6 @@ namespace Lib\SwiftMailer;
  */
 class Init
 {
-    /** Twigのテンプレートディレクトリseparatorはいらない */
-    const TEMPLATE_DIR = 'mail';
-
-    /** Twigのキャッシュディレクトリseparatorはいらない */
-    const CACHE_DIR = 'cache';
-
     /** クラスの保存先 最後にseparatorはいらない */
     const LOG_DIR = 'logs/mail';
 
@@ -197,28 +191,6 @@ class Init
             $this->path,
             $current,
             FILE_APPEND
-        );
-    }
-
-    /**
-     * Twigのテンプレートを反映して返す
-     *
-     * @param String $template // テンプレートファイル名
-     * @param Array $data // テンプレート内ファイルの変数
-     * @return void
-     * @codeCoverageIgnore
-     */
-    public function setTemplate($template, $data)
-    {
-        $dir = self::TEMPLATE_DIR;
-        $loader = new \Twig_Loader_Filesystem($dir);
-        $this->twig = new \Twig_Environment($loader, array(
-            'cache' => self::CACHE_DIR
-        ));
-
-        return $this->twig->render(
-            $template,
-            $data
         );
     }
 
