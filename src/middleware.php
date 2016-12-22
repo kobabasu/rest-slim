5e17842c7e3a3747a53fe646e7f85b6322bbaf81
kobabasu <keiji.kobayashi.web@gmail.com>
2016-12-23 06:36:53 +0900

    デフォルトでbasic認証を外す

--- src/middleware.php
+++ src/middleware.php
@@ -11,10 +11,13 @@ $c = $app->getContainer();
 
 /**
  * tuupola/slim-basic-auth
+ * disable as default
  */
+/*
 $app->add(new HttpBasicAuthentication([
     'path' => $c->get('auth')['basic_auth_path'],
     'secure' => false,
     'relaxed' => array('localhost', '127.0.0,1'),
     'users' => $c->get('auth')['basic_auth']
 ]));
+ */
