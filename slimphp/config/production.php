61ca45280c5b80d4a670d879ff70dc6ead6d2bf3
kobabasu <keiji.kobayashi.web@gmail.com>
2018-07-08 10:20:26 +0900

    コピー用のslimphpディレクトリを作成

---
+++ slimphp/config/production.php
@@ -0,0 +1,33 @@
+<?php
+const ENVIRONMENT_MODE = 'production';
+const DEBUG_DEFAULT    = false;
+
+// can use array as const in php5.6 or upper
+const ENABLE_BASIC_AUTH  = false;
+const BASIC_AUTH_USER    = 'seiwa';
+const BASIC_AUTH_PASS    = 'seiwa012';
+
+/*
+ * Database
+ */
+const DB_HOST       = '0.0.0.0';
+const DB_NAME       = 'ad111a0v1a_core';
+const DB_USER       = 'ad111a0v1a';
+const DB_PASS       = 'xLxrCvNW';
+const DB_PORT       = '3307';
+const DB_CHARSET    = 'utf8';
+
+/*
+ * Mailer
+ */
+const MAIL_HOST     = '127.0.0.1';
+const MAIL_PORT     = '1025';
+const MAIL_USER     = null;
+const MAIL_PASS     = null;
+
+const MAIL_FROM     = 'system@seiwa-chemical.net';
+const MAIL_NAME     = 'System Notification';
+const MAIL_ADDR     = 'system@seiwa-chemical.net';
+
+// return-path
+const MAIL_FAIL     = 'system@seiwa-chemical.net';
